668b0d65322ed9722bccf66d57961424cf58381e
Denilson4lmeida <denilsonalmeida484@gmail.com>
2025-03-13 22:27:45 -0300

    Salvando alterações antes do pull

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const connection = mysql.createConnection({
-    host: 'localhost', // ou o IP do seu servidor
-    user: process.env.DB_USER, 
+    host: process.env.DB_HOST, // Endereço do banco de dados remoto
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
